2190d9979c2823cb12c9ffa48d6ba43922086691
Facundo David Jimenez <fcndojmnz@gmail.com>
2026-05-26 00:34:19 -0300

    primer punto

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        
+        Console.WriteLine("Hello, World!");
+        int a; 
+        int b; 
+        a=10; 
+        b=a; 
+        Console.WriteLine("valor de a:"+a); 
+        Console.WriteLine("valor de b:"+b); 
+
+        Console.Write("Por favor, ingrese un número: ");
+        string? entrada = Console.ReadLine();
+
+        int numero = 0; 
+        bool esNumero = int.TryParse(entrada, out numero);
+
+        if (esNumero)
+        {
+         
+            if (numero > 0)
+            {
+                int numeroInvertido = 0;
+                int auxiliar = numero;
+
+                // Ciclo iterativo while (pág. 44)
+                while (auxiliar > 0)
+                {
+                    int ultimoDigito = auxiliar % 10;
+                    numeroInvertido = (numeroInvertido * 10) + ultimoDigito;
+                    auxiliar = auxiliar / 10;
+                }
+
+                Console.WriteLine("El número invertido es: " + numeroInvertido);
+            }
+            else
+            {
+                Console.WriteLine("Error: El número debe ser mayor a 0.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error: El texto ingresado no es un número válido.");
+        }
+    }
+}
